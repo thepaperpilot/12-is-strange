e0063f3f3f66e4bec53a2982eb380dc4ecca4c95
thepaperpilot <thepaperpilot@gmail.com>
2015-10-23 20:12:21 -0500

    initial libgdx commit

---
+++ desktop/src/thepaperpilot/strange/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package thepaperpilot.strange.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import thepaperpilot.strange.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
