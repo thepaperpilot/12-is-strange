df5b1164f5166392b8b1c1a5482155d816c77b26
thepaperpilot <thepaperpilot@gmail.com>
2015-10-27 22:59:17 -0500

    Cleaned up the imports

--- core/src/thepaperpilot/strange/Dialogue.java
+++ core/src/thepaperpilot/strange/Dialogue.java
@@ -1,10 +1,7 @@
 package thepaperpilot.strange;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
