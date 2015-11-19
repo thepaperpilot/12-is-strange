6a513623ce97f225708173872081cede5ae8e64b
thepaperpilot <thepaperpilot@gmail.com>
2015-11-19 13:27:55 -0600

    Commented Level.java

--- core/src/thepaperpilot/strange/Levels/Level.java
+++ core/src/thepaperpilot/strange/Levels/Level.java
@@ -17,14 +17,17 @@ public class Level {
     Map<String, Item> items = new HashMap<String, Item>();
 
     public Level(LevelPrototype levelPrototype) {
+        // load all the different areas
         for (Scene.ScenePrototype scene : levelPrototype.scenes) {
             scenes.put(scene.name, new Scene(scene, this));
         }
 
+        // load all the different items
         for (Item.ItemPrototype item : levelPrototype.items) {
             items.put(item.name, new Item(item, this));
         }
 
+        // load all the different recipes
         for (Item.CombinationPrototype prototype : levelPrototype.combinations) {
             Item[] key = new Item[prototype.ingredients.length];
             for (int i = 0; i < prototype.ingredients.length; i++) {
@@ -33,18 +36,23 @@ public class Level {
             combinations.put(key, this.items.get(prototype.result));
         }
 
+        // go to the first area
         firstScene = scenes.get(levelPrototype.first);
     }
 
     public static Level readLevel(String fileName) {
+        // read the level from a JSON file
         return new Level(json.fromJson(LevelPrototype.class, Gdx.files.internal(fileName)));
     }
 
     public void combine() {
+        // iterate through all the recipes in this level
         outer:
         for (int i = 0; i < combinations.keySet().size(); i++) {
             Item[] combo = (Item[]) combinations.keySet().toArray()[i];
+            // ensure we have the same amount of items selected as the recipe needs
             if (selected.size() != combo.length) continue;
+            // check if we have the same items selected as the recipe needs
             for (Item item : selected) {
                 boolean found = false;
                 for (Item comboItem : combo) {
@@ -53,6 +61,8 @@ public class Level {
                 }
                 if (!found) continue outer;
             }
+
+            // if the selected items match the recipe, complete the recipe
             Main.manager.get("audio/pickup.wav", Sound.class).play();
             ArrayList<Item> tmp = new ArrayList<Item>();
             Collections.addAll(tmp, combo);
